ee7ea30a9a71bd3e6a27385a5c6131694df4209a
yazdansafarii <yazdansafarii@gmail.com>
2025-10-26 21:24:39 +0330

    change static root
    STATIC_ROOT = BASE_DIR / 'statics'

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'statics'
 
 
 MEDIA_URL = 'media/'
