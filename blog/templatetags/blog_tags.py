ecedd6c51f711028261fa46b92d2067a141e18a8
yazdansafarii <yazdansafarii@gmail.com>
2025-10-31 17:09:54 +0330

    Add dynamic popular posts widget to blog
    Introduced a custom template tag to render popular posts dynamically based on view count. Updated templates to use the new tag and refactored the popular posts widget to display actual post data, including images and titles. Adjusted CSS for image sizing in the widget.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,11 @@
 from django import template
 
 register = template.Library()
+from blog.models import post
+
+
+@register.inclusion_tag('blog/popular-posts-blog.html')
+
+def popular_posts():
+    posts = post.objects.order_by('counted_view')
+    return {'posts':posts}
