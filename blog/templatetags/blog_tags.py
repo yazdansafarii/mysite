bdd14f8aa4935b6148abd1db7535b323d8c1e305
yazdansafarii <yazdansafarii@gmail.com>
2025-11-02 19:37:57 +0330

    add tags with inclution

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 from blog.models import post
+from blog.models import category
 
 
 @register.inclusion_tag('blog/popular-posts-blog.html')
@@ -9,3 +10,14 @@ from blog.models import post
 def popular_posts():
     posts = post.objects.order_by('counted_view')
     return {'posts':posts}
+
+
+@register.inclusion_tag('blog/post-catgory-blog.html')
+
+def Category():
+    posts = post.objects.filter(status=1)
+    categorys = category.objects.all()
+    cat_dic = {}
+    for name in categorys:
+        cat_dic[name]=posts.filter(category=name).count()
+    return{'categorys':cat_dic}
